220ad84479dd1f1666f0970f5834546f1745f9fb
hungco2001 <hungco10011001@gmail.com>
2021-12-27 20:25:07 +0700

    sua header

--- pages/header.php
+++ pages/header.php
@@ -1,23 +1,128 @@
-<div class="header">
-            <div class="row justify-content-md-center ">
+<body class="">
+    <div class="container-fluid ps-0 pe-0">
+        <div class="header">
+            <ul class="nav bg-dark">
+                <li class="nav-item ms-5 bg-light">
+                    <a class="nav-link  text-dark item" href="#">Khám Phá</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link item" href="#">Đặt Hàng</a>
+                </li>
+                <li class="nav-item position-relative">
+                    <a class="nav-link item " href="#">Đi Chợ</a>
+                    <img src="img/sb-new.gif" alt="">
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link item" href="#">NowPOS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link item" href="#">Giảm Giá</a>
+                </li>
+            </ul>
+            <!--header 1  -->
 
-                <div class="col-md-auto">
-                    <a href="http://www.foody.vn/ung-dung-mobile" title="Foody App trên Mobile" style="text-decoration: none;">
-                        <img src="https://images.foody.vn/biz_banner/foody-675x355_foodyappbanner-636530746968443602.jpg">
-
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="ms-5" href="#">
+                        <div class="logo">
+                            <img src="img/logo-foody.png" alt="">
+                        </div>
                     </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item dropdown border ms-2">
+                                <a class="nav-link dropdown-toggle" href="#" id="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Hà Nội
+                                </a>
+                                <ul class="dropdown-menu pt-0 pb-0 " aria-labelledby="">
+                                    <form class="d-flex ">
+                                        <input class="form-control me-2" type="search" placeholder="Tỉnh thành " aria-label="Search">
+                                    </form>
+                                    <li><a class="dropdown-item" href="#">TP.HCM</a></li>
+                                    <li><a class="dropdown-item" href="#">Hà Nội</a></li>
+                                    <li><a class="dropdown-item" href="#">Đà Nẵng</a></li>
+                                    <li><a class="dropdown-item" href="#">Cần Thơ</a></li>
+                                    <li><a class="dropdown-item" href="#">Khánh Hòa</a></li>
+                                    <li><a class="dropdown-item" href="#">Vũng Tàu</a></li>
+                                    <li><a class="dropdown-item" href="#">Hải Phòng</a></li>
+                                    <li><a class="dropdown-item" href="#">Bình Thuận</a></li>
+                                    <li><a class="dropdown-item" href="#">Lâm Đồng</a></li>
+                                    <li><a class="dropdown-item" href="#">Đồng Nai</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown border ms-1 me-1">
+                                <a class="nav-link dropdown-toggle" href="#" id="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Ăn Uống
+                                </a>
+                                <ul class="dropdown-menu pt-0 pb-0" aria-labelledby="">
+                                    <li><a class="dropdown-item" href="#">TP.HCM</a></li>
+                                    <li><a class="dropdown-item" href="#">Hà Nội</a></li>
+
+                                </ul>
+                            </li>
+                            <form class="d-flex search">
+                                <input class="form-control me-2" type="search" placeholder="Địa điểm,món ăn,loại hình " aria-label="Search">
+                                <button class="btn btn-outline-success " type="submit">
+                                    <i class="bi bi-search"></i>
+                                </button>
+                            </form>
+                            <li class="nav-item dropdown border ms-4">
+                                <a class="nav-link " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi bi-list text-dark"></i>
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Ở đâu</a></li>
+                                    <li><a class="dropdown-item" href="#">Giao hàng</a></li>
+                                    <li><a class="dropdown-item" href="#">Ăn gì</a></li>
+                                    <li><a class="dropdown-item" href="#">Sưu Tập</a></li>
+                                    <li><a class="dropdown-item" href="#">Bộ sưu tập hình ảnh</a></li>
+                                    <li><a class="dropdown-item" href="#">Coupons</a></li>
+                                    <li><a class="dropdown-item" href="#">TV</a></li>
+                                    <li><a class="dropdown-item" href="#">Bình Luận</a></li>
+                                    <li><a class="dropdown-item" href="#">Blogs</a></li>
+                                    <li><a class="dropdown-item" href="#">Khuyến mãi</a></li>
+                                    <li><a class="dropdown-item" href="#">Miễn phí</a></li>
+                                    <li><a class="dropdown-item" href="#">Top thành viên</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item ms-5">
+                                <a class="nav-link " href="login.php" role="button" >
+                                    Đăng Nhập
+                                </a>
+                            </li>
+                            <li class="nav-item dropdown ms-1 ">
+                                <a class="nav-link " href="#" id="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi bi-bell-fill"></i>
+                                </a>
+                                <ul class="dropdown-menu pt-0 pb-0" aria-labelledby="">
+                                    <ul class="nav nav-tabs">
+                                        <li class="nav-item">
+                                            <a class="nav-link active" aria-current="page" href="#">Dịch vụ</a>
+                                        </li>                                        
+                                        <li class="nav-item">
+                                            <a class="nav-link active" aria-current="page" href="#">Của tôi</a>
+                                        </li>    
+                                    </ul>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown ms-1 ">
+                                <a class="nav-link " href="#" id="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i class="bi bi-plus-lg "></i>
+                                </a>
+                                <ul class="dropdown-menu pt-0 pb-0" aria-labelledby="">
+                                    <li><a class="dropdown-item" href="#">TP.HCM</a></li>
+                                    <li><a class="dropdown-item" href="#">Hà Nội</a></li>
+
+                                </ul>
+                            </li>
+                        </ul>
+
+                    </div>
                 </div>
-
-            </div>
-
-            <div class="row banner-caption ">
-
-                <div class="banner-caption-name ">
-                    Foody App trên Mobile
-                </div>
-                <div class="banner-caption-desc">
-                    <p>Khám phá, đặt bàn, giao tận nơi</p>
-                </div>
-            </div>
-
+            </nav>
         </div>
+    </div>
+</body>
---
+++ pages/slider.php
@@ -0,0 +1,23 @@
+<div class="slider">
+            <div class="row justify-content-md-center ">
+
+                <div class="col-md-auto">
+                    <a href="http://www.foody.vn/ung-dung-mobile" title="Foody App trên Mobile" style="text-decoration: none;">
+                        <img src="https://images.foody.vn/biz_banner/foody-675x355_foodyappbanner-636530746968443602.jpg">
+
+                    </a>
+                </div>
+
+            </div>
+
+            <div class="row banner-caption ">
+
+                <div class="banner-caption-name ">
+                    Foody App trên Mobile
+                </div>
+                <div class="banner-caption-desc">
+                    <p>Khám phá, đặt bàn, giao tận nơi</p>
+                </div>
+            </div>
+
+        </div>
